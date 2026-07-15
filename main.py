ba371a8f970cb1e1735a8e139056240c5198e1d8
depth808 <bryanoost@gmail.com>
2026-07-14 23:20:45 -0600

    draw player

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+
+import player
 from constants import *
 from logger import log_state
 
@@ -11,15 +13,18 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0.0
-    
+    player_ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 return
         screen.fill("black")
+        player_ship.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        
+
+
 if __name__ == "__main__":
     main()
